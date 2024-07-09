8c662000704f0a223af86eb61f36c029ca5f6e72
DreaMeR <DreaMeR@DREAMER>
2024-07-09 10:24:34 +0600

    Добавьте файлы проекта.

---
+++ Question/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте!");
+            Console.WriteLine(" ");
+
+            Console.Write("Как Вас зовут? ");
+            string name = Console.ReadLine();
+
+            Console.Write("Сколько Вам лет? ");
+            string age = Console.ReadLine();
+
+            Console.Write("А какой у Вас любимый десерт? ");
+            string dessert = Console.ReadLine();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine($"Вы {name}, Вам {age} лет и Ваш любимый десерт это {dessert}");
+
+            Console.ReadKey();
+        }
+    }
+}
